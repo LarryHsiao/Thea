2695451f263a6cf0e237d47a8f098d88200721c6
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-12-27 13:35:03 +0800

    Update test assets

--- src/test/groovy/com/larryhsiao/thea/version/VersionIntTest.groovy
+++ src/test/groovy/com/larryhsiao/thea/version/VersionIntTest.groovy
@@ -6,35 +6,31 @@ class VersionIntTest extends GroovyTestCase {
     void testVer0() {
         assertEquals(
                 0,
-                new VersionInt(
-                        new ConstSource<String>("v0.0.0")
-                ).value()
+                new VersionInt(new ConstSource<String>("v0.0.0")).value()
         )
     }
 
     void testVerNon0() {
         assertNotSame(
                 0,
-                new VersionInt(
-                        new ConstSource<String>("v0.0.1")
-                ).value()
+                new VersionInt(new ConstSource<String>("v0.0.1")).value()
         )
     }
 
     void testVerNonPrefixV() {
         assertNotSame(
                 0,
-                new VersionInt(
-                        new ConstSource<String>("0.0.1")
-                ).value()
+                new VersionInt(new ConstSource<String>("0.0.1")).value()
+        )
+        assertEquals(
+                1,
+                new VersionInt(new ConstSource<String>("0.0.1")).value()
         )
     }
 
     void testInvalidedStringHasException() {
         try {
-            new VersionInt(
-                    new ConstSource<String>("INVALIDED")
-            ).value()
+            new VersionInt(new ConstSource<String>("INVALIDED")).value()
             fail()
         } catch (IllegalArgumentException e) {
             assertTrue(true)
@@ -44,30 +40,21 @@ class VersionIntTest extends GroovyTestCase {
     void testMajorNumber() {
         assertEquals(
                 10000,
-                new VersionInt(
-                        new ConstSource<String>("v1.0.0")
-                ).value()
+                new VersionInt(new ConstSource<String>("v1.0.0")).value()
         )
     }
-
 
     void testMinorNumber() {
         assertEquals(
                 100,
-                new VersionInt(
-                        new ConstSource<String>("v0.1.0")
-                ).value()
+                new VersionInt(new ConstSource<String>("v0.1.0")).value()
         )
     }
-
 
     void testPatchNumber() {
         assertEquals(
                 1,
-                new VersionInt(
-                        new ConstSource<String>("v0.0.1")
-                ).value()
+                new VersionInt(new ConstSource<String>("v0.0.1")).value()
         )
     }
-
 }
